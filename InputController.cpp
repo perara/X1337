dded63cac3d8767ff9d3cda56f633d14b4b13e4d
Gunnar Sebastian Abusdal <gunnar.abusdal@getmail.no>
2013-10-11 18:56:08 +0200

    initial commit fix

--- InputController.cpp
+++ InputController.cpp
@@ -3,14 +3,19 @@
 void InputController::updateWorld()
 {
 	sf::Event event;
-	sf::Mouse::getPosition(window).x;
+
 	if(sf::Mouse::getPosition(window).x>0
 		&& sf::Mouse::getPosition(window).x<w.windowSize.x-(w.p1.playerShape.getRadius()*2))
 			w.p1.setPlayerPosX(sf::Mouse::getPosition(window).x);
 
-	if(	sf::Mouse::getPosition(window).y>0
+	if(sf::Mouse::getPosition(window).y>0
 		&& sf::Mouse::getPosition(window).y<w.windowSize.y-(w.p1.playerShape.getRadius()*2))
 			w.p1.setPlayerPosY(sf::Mouse::getPosition(window).y);
+
+	if(sf::Mouse::isButtonPressed)
+	{
+
+	}
 
 	while (window.pollEvent(event))
     {
--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
 	World* w = new World();
 	Renderer* r = new Renderer(*w);
 	InputController* i = new InputController(*w, r->window);
+	sf::Mouse::setPosition();
 
 	// run the program as long as the window is open
     while (r->isWindowOpen())
